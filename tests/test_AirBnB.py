1a136e5e501944faccaf2886e086aec94a3d5ba9
root <869@holbertonschool.com>
2019-11-10 23:44:37 +0000

    uploading the first tests

---
+++ tests/test_AirBnB.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Unittester for the AirBnB project
+"""
+import unittest
+import pep8
+from models.base_model import BaseModel
+
+
+class Tester_forAirBnB(unittest.TestCase):
+    """ Tester that checks whetever everything is working as intended """
+
+
+    def test_pep8_pycodestyle(self):
+        """ Tester for pep8/pycodestyle """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors, please fix them")
+
+    def test_instanceof_itself(self):
+        """ Tester to see if he is an instance of BaseModel """
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        self.assertTrue(isinstance(my_model, BaseModel))
+
+    def test_checkif_docstring(self):
+        """ Tester to see if the function is correctly documented """
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
